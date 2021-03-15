84588a360a115160a6f2f4c6dcbe43567c5d352a
aryanarya1671 <aryanarya.1671@gmail.com>
2021-03-16 00:36:53 +0530

    basic programmes added

---
+++ MyBio.java
@@ -0,0 +1,24 @@
+/**
+* Name : Aryan
+* Loaction : Bihar
+* Contact : 8969332625
+* Email : 912995@cognizant.com
+*/
+
+class MyBio {
+ public static void main(String [] args) {
+	 
+	 int personId=1001;
+	 String personName="Aryan";
+	 String location="Bihar";
+	 String contactNumber="8969332625";
+	 String email="912995@cognizant.com";
+	 
+	 
+   System.out.println(id : "+personId);
+   System.out.println("Name : "+personName);
+   System.out.println("Loaction : "+location);
+   System.out.println("Contact : "+contactNumber);
+    System.out.println("Email : "+email);
+   }
+   }
