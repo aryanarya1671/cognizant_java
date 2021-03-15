fe4b9684081b55ca66f57e74732416eef88e9775
aryanarya1671 <aryanarya.1671@gmail.com>
2021-03-16 00:23:49 +0530

    first program added

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+/**
+* Name : Aryan
+* Description : Compilation & running Test
+* Date : 13/3/2021
+*/
+
+class HelloWorld {
+ public static void main(String [] args){
+   System.out.println("HelloWorld");
+   }
+   }
